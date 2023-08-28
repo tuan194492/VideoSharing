259191afe0ed67941a600a1a3a71b735a6d8257d
tuan194492 <tuan1216302001@gmail.com>
2023-08-28 11:16:20 +0700

    Add swagger docs for api

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const cors = require('cors');
 const fileUpload = require('express-fileupload')
 const route = require('./routes/index');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const {databaseInit} = require("../src/utils/database/index")
 
@@ -17,6 +19,40 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(fileUpload({
   limits: { fileSize: 50 * 1024 * 1024 },
 }));
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+    description:
+      'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+    license: {
+      name: 'Licensed Under MIT',
+      url: 'https://spdx.org/licenses/MIT.html',
+    },
+    contact: {
+      name: 'JSONPlaceholder',
+      url: 'https://jsonplaceholder.typicode.com',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Development server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 databaseInit();
 route(app);
--- back-end/src/routes/authRoute.js
+++ back-end/src/routes/authRoute.js
@@ -2,8 +2,37 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController')
 
-
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login
+ *     description: Login
+ *     requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          email:
+ *                              type: string
+ *                              description: The user's email.
+ *                              example: abc@gmail.com
+ *                          password:
+ *                              type: string
+ *                              description: The user's password.
+ *                              example: 123456
+ *     responses:
+ *          201: 
+ *              description: Success
+ *          500:
+ *              description: Internal Server error
+ *           
+ * 
+*/
 router.post('/login', authController.login);
+
 router.post('/register', authController.register);
 
 module.exports = router;
