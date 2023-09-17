4de2d10a127af57ce47bb13a2c20cf727fee4fd5
bibihehe <anh.bd194216@sis.hust.edu.vn>
2023-09-17 16:40:53 +0700

    1

--- back-end/src/utils/database/sequelize.js
+++ back-end/src/utils/database/sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("video_sharing", "root", "#Tuan267.", {
+const sequelize = new Sequelize("video_sharing", "root", "gapo0903", {
     host: "localhost",
     dialect: "mysql",
   });
